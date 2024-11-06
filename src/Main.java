4c9d540c4c718914b626bc4338a15e504bbe4e28
BT_2S14_04 <gsuih@gmail.com>
2024-11-06 11:06:02 -0500

    d

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Welcome to Uncanon ACOTAR, a choice-based text game based off of the hit book series....\nA COURT OF THORNS AND ROSES by Sarah J. Maas!");
+        System.out.println("Feyre hated having to be the sole provider of her family. Her sisters felt they were far too" +
+                "self-important to get down to the dirty work and her father, despite his almost pathetic efforts, was" +
+                "not much more of a use. A few years back, his leg had been broken" +
+                "by loan collectors that didn't care about the three pairs of young eyes watching their father get beat." +
+                "Feyre always blamed him for making a stupid investment that costed all their money, and she only felt" +
+                "a tinge of pity for the man " +
+                "It was all up to her, and although she never outwardly said it, she wanted a life better than poverty");
     }
 }
